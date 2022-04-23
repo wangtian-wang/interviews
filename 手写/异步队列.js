50dccefa28d835750404a7e64fbbec6bf5b005fd
wangtian <1434215837@qq.com>
2022-04-23 20:23:12 +0800

    添加手写异步队列控制

---
+++ 手写/异步队列.js
@@ -0,0 +1,47 @@
+class TaskQueue {
+  constructor() {
+    this.max = 10;
+    this.taskList = [];
+    setTimeout(() => {
+      this.run();
+    }, 0);
+  }
+  addTask(task) {
+    this.taskList.push(task);
+  }
+  run() {
+    const len = this.taskList.length;
+    if (!len) {
+      return;
+    }
+    const min = Math.min(len, this.max);
+    for (let i = 0; i < min; i++) {
+      this.max--; // 开始占用一个任务空间
+      const task = this.taskList.shift();
+      task()
+        .then((res) => {})
+        .catch((err) => {
+          console.log(err);
+        })
+        .finally(() => {
+          this.max++; // 开始释放一个任务空间
+          this.run(); // 自动执行下一轮任务
+        });
+    }
+  }
+}
+// test
+function createTask(i) {
+  return () => {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve(i);
+      }, 2000);
+    });
+  };
+}
+const taskQueue = new TaskQueue();
+for (let i = 0; i < 20; i++) {
+  const task = createTask(i);
+  taskQueue.addTask(task);
+}
