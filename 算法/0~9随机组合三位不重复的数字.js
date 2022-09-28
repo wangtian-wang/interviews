f6a8746e253434a5634ee2a5c29e64f56853439c
wangtian <1434215837@qq.com>
2022-09-28 10:57:23 +0800

    修改随机组合的初始值为0

--- 算法/0~9随机组合三位不重复的数字.js
+++ 算法/0~9随机组合三位不重复的数字.js
@@ -1,5 +1,6 @@
 let count = 0;
-for (let i = 1; i < 10; i++) {
+// 每轮循环的i都应该从0开始
+for (let i = 0; i < 10; i++) {
   for (let j = 0; j < 10; j++) {
     if (j === i) continue;
     for (let k = 0; k < 10; k++) {
