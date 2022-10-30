4d29db8db4c54c3044873ff34c9a46c3292d57f8
wangtian <1434215837@qq.com>
2022-10-30 17:00:24 +0800

    promise.retry的详细注释

--- js基础/es6/promise/promise手写/promise.retry.js
+++ js基础/es6/promise/promise手写/promise.retry.js
@@ -9,7 +9,6 @@ Promise.retry = function (fn, limit) {
     // limit = 5
     while (limit--) {
       // limit -- ;limit = 4
-      console.log("---");
       try {
         let result = await fn();
         resolve(result);
@@ -23,41 +22,43 @@ Promise.retry = function (fn, limit) {
     }
   });
 };
+Promise.retryAsync = function retryAsync(fn, limit) {
+  if (typeof limit !== "number") limit = 3;
+  return new Promise((resolve, reject) => {
+    let index = 0;
+    async function inner() {
+      index++;
+      try {
+        let res = await fn();
+        resolve(res);
+      } catch (err) {
+        // console.log(index, "index");
+        if (index < limit) {
+          inner();
+        } else {
+          reject(err);
+        }
+      }
+    }
+    inner();
+  });
+};
 // test example
 function getNum() {
   return new Promise((resolve, reject) => {
     const num = Math.random() * 0.5;
     setTimeout(() => {
-      if (num > 0.4) {
+      if (num > 0.1) {
         resolve(`${num}ok`);
       } else {
-        // console.log("failing" + num);s
-        reject(num);
+        reject(undefined);
       }
     }, 1000);
-
-    /**
-        if (num > 0.3) {
-      resolve("success");
-    } else {
-      console.log("----------");
-      reject("failing");
-    }
-    */
   });
 }
 
-Promise.retry(getNum, 5).then(
-  (res) => {
-    console.log(res);
-  },
-  (error) => {
-    console.log(error, "finally error");
-  }
-);
-
 /**
- *   将fn执行time次,返回每次fn执行的结果
+ *   将fn 一共 执行time次,  执行完time后 ,返回每次fn执行的结果
  */
 Promise.retryAll = (fn, time) => {
   return new Promise((resolve, reject) => {
@@ -69,7 +70,7 @@ Promise.retryAll = (fn, time) => {
       fn().then(
         (res) => {
           if (i === time - 1) {
-            resolve(result);
+            resolve(result); // 执行resolve后 后面的代码  result[i] = res;还能执行
           }
           result[i] = res;
         },
@@ -84,6 +85,41 @@ Promise.retryAll = (fn, time) => {
   });
 };
 
+/**
+ *  总共尝试n次,  哪次成功, 就返回哪次成功之前 函数执行的所有结果
+ *               n次都是失败的 则返回所有失败的结果
+ */
+Promise.retryAllAsync = function retryAllAsync(fn, limit) {
+  return new Promise((resolve, reject) => {
+    let result = [],
+      index = 0;
+    async function run() {
+      // 只要run函数 执行了一次 就代表当前的limit次数 少了一次
+      let curIndex = index++;
+      try {
+        let res = await fn();
+        result[curIndex] = res;
+        resolve(result);
+      } catch (err) {
+        result[curIndex] = err;
+        if (index < limit) {
+          run();
+        } else {
+          reject(result);
+        }
+      }
+    }
+    run();
+  });
+};
+Promise.retryAll(getNum, 5).then(
+  (res) => {
+    console.log(res);
+  },
+  (error) => {
+    console.log(error, "finally error");
+  }
+);
 /** 发送一个请求，1s后就返回失败的结果 */
 
 function getData() {
