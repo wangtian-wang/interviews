f0e471c8fe30774c97fb109d88997992c1d33495
wangtian <1434215837@qq.com>
2022-10-30 17:02:32 +0800

    新增加判断promise的方法

--- js基础/es6/promise/promise手写/check-promise.js
+++ js基础/es6/promise/promise手写/check-promise.js
@@ -2,15 +2,23 @@ const checkPromise = (fn) => {
   if (fn !== null && typeof fn === "function") {
     var then;
     try {
-      then = fn.then;
+      then = fn().then;
     } catch (err) {
       return false;
     }
-    if (typeof fn === "function") return true;
+    if (typeof then === "function") return true;
   } else {
     return false;
   }
 };
+function test() {
+  return new Promise((resolve, reject) => resolve());
+}
+function isPromise(p) {
+  return p && Object.prototype.toString.call(p) === "[object Promise]";
+}
+let res = checkPromise(test);
+console.log(isPromise(test()), "-----");
 module.exports = {
   checkPromise,
 };
